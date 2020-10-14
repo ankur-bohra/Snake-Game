82ebddb89f676ff28f99883c381b6854726bca1c
ankurbohra04 <ankurbohra04@gmail.com>
2020-10-14 10:29:05 +0530

    Add difficulty mode screen

--- src/interface.py
+++ src/interface.py
@@ -20,6 +20,35 @@ def switchandplay(menu, command):
         command()
     return callback
 
+def displaymodes(window, menu, play):
+    def modes_builder():
+        menu.pack_forget()
+        modes = Frame(window, bg="black")
+        modes.pack(fill=BOTH, expand=True)
+
+        mode_title = Label(modes,
+                           text="Game Mode", font=("Arial", 30, "bold"),
+                           fg="green", bg="black")
+        mode_title.place(relx=0.3, rely=0.2)
+
+        def playmode(mode_step):
+            def switchandplay():
+                modes.pack_forget()
+                play(window, mode_step)
+            return switchandplay
+
+        for index in range(len(GAME_MODES)):
+            mode = GAME_MODES[index]
+            step = GAME_STEPS[index]
+            button = Button(modes,
+                            borderwidth=3, relief=RIDGE,
+                            text=mode, font=("Arial", 20, "bold"),
+                            fg="black", bg="green", activebackground="light green",
+                            width=9,
+                            command=playmode(step))
+            button.place(relx=.34, rely=0.4 + index*0.12)
+    return modes_builder
+
 
 def displaymenu(window, play):
     menu = Frame(window, bg="black")
@@ -27,14 +56,14 @@ def displaymenu(window, play):
     game_title = Label(menu,
                        text="Snake Game", font=("Arial", 30, "bold"),
                        fg="green", bg="black")
-    game_title.place(relx=0.25, rely=0.2)
+    game_title.place(relx=0.29, rely=0.2)
 
     play_btn = Button(menu,
                       borderwidth=3, relief=RIDGE,
                       text="Play", font=("Arial", 20, "bold"),
                       fg="black", bg="green", activebackground="light green",
                       width=7,
-                      command=switchandplay(menu, play))
+                      command=displaymodes(window, menu, play))
     play_btn.place(relx=.375, rely=0.4)
 
     scores_btn = Button(menu,
