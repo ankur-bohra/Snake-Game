b9f7d177debbc40afc106f7ec1ae1f3bb6aa512a
ankurbohra04 <ankurbohra04@gmail.com>
2020-10-14 10:28:39 +0530

    Reorder, add food points

--- src/settings.py
+++ src/settings.py
@@ -3,17 +3,18 @@ All adjustable game settings
 '''
 GRID_SIZE = 20 # cells
 CELL_SIZE = 30 # px
+GRID_COLOR = "black"
 
 MIN_LENGTH = 5 # cells
-MIN_STEP = 330 # ms
+BODY_COLOR = "green" # body + tail
+HEAD_COLOR = "dark green"
+
+GAME_MODES = ["Easy", "Moderate", "Difficult", "Hard"]
+GAME_STEPS = [250, 200, 150, 100] # corresponding to GAME_MODES
 
 FOOD_CELLS_PRESENT = 3 # at a time
 FOOD_GEN_STEP_MIN = 2000 # between eating and replacement
 FOOD_GEN_STEP_MAX = 4000
-FOOD_STEP_LOSSES = [10, 15, 20]
+FOOD_POINTS = [10, 15, 20]
 FOOD_RARITY = [0.5, 0.3, 0.2] # probability, corresponding to FOOD_STEP_LOSSES, multiples of 0.1
-
-GRID_COLOR = "black"
-BODY_COLOR = "green" # body + tail
-HEAD_COLOR = "dark green"
 FOOD_COLORS = ["red", "orange", "yellow"] # corresponding to FOOD_STEP_LOSSES
