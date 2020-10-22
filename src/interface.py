cbc197cbd105e46867aac541fac311da6a54b5c8
ankurbohra04 <ankurbohra04@gmail.com>
2020-10-22 14:52:49 +0530

    Add score perstistence, player filter

--- src/interface.py
+++ src/interface.py
@@ -1,12 +1,12 @@
 '''
 Menu and other interface of game
 '''
+import json
 from tkinter import *
 from core import playgame
 from settings import *
 
 window = None
-scores = {}
 screens = []
 
 def makewindow():
@@ -23,7 +23,7 @@ def clearscreens():
     for screen in screens:
         screen.destroy()
 
-def endgame(score, mode_step):
+def endgame(mode_step, score):
     # Show a dying screen
     game = screens[-1]
     window.unbind("<KeyPress>")
@@ -41,14 +41,30 @@ def endgame(score, mode_step):
                        fg="grey", bg="black")
     cont_label.place(relx=0.3, rely=0.85)
 
+    player_label = Label(game,
+                         text="Player:", font=("Arial", 15),
+                         fg="white", bg="black")
+    player_label.place(relx=0.45, rely=0.6, anchor=E)
+
+    player_entry = Entry(game,
+                         font=("Arial", 15),
+                         width=15)
+    player_entry.place(relx=0.48, rely=0.6, anchor=W)
+    player_entry.insert(0, "Unknown")
+
     def resume(key):
-        global scores
         if key.keysym == "Return":
             if score > 0:
-                mode = GAME_MODES[GAME_STEPS.index(mode_step)]
-                mode_scores = scores[mode]
-                mode_scores.append(score)
-                scores[mode] = mode_scores
+                with open("src/scores.json", "r+") as scores_file:
+                    mode = GAME_MODES[GAME_STEPS.index(mode_step)]
+
+                    scores = json.load(scores_file)
+                    scores[mode].update({score: player_entry.get().title()})
+
+                    scores_file.seek(0)
+                    scores_file.truncate()
+                    json.dump(scores, scores_file)
+
             clearscreens()
             displaymenu()
     window.bind("<Return>", resume)
@@ -60,7 +76,7 @@ def displaycredits(frame):
     credits_label.place(relx=0.5, rely=0.935, anchor=CENTER)
 
 def displaymodes():
-    global window, scores
+    global window
     clearscreens()
 
     modes_screen = Frame(window, bg="black")
@@ -75,7 +91,6 @@ def displaymodes():
 
     def playmode(mode_step):
         def command():
-            global scores
             modes_screen.pack_forget()
             game = playgame(window, mode_step, endgame)
             screens.append(game)
@@ -101,97 +116,128 @@ def displaymodes():
     back_button.place(relx=0.03, rely=0.9)
 
 def displayscore():
-    def showmode(mode_no):
-        global window, scores
+    def showmode(mode_no, filter_player):
+        global window
         if mode_no > len(GAME_MODES) - 1:
             mode_no = 0
         mode = GAME_MODES[mode_no]
-        scores[mode].sort(reverse=True)
-        scores[mode] = scores[mode][:7] # upto 7 highscores stored
+        with open("src/scores.json", "r+") as scores_file:
+            scores = json.load(scores_file)
+            sorted_scores = list(scores[mode])
+            sorted_scores.sort(reverse=True)
+            sorted_scores = sorted_scores[:7] # upto 7 highscores shown
 
-        clearscreens()
+            clearscreens()
 
-        score_screen = Frame(window, bg="black")
-        screens.append(score_screen)
-        displaycredits(score_screen)
-        score_screen.pack(fill=BOTH, expand=True)
+            score_screen = Frame(window, bg="black")
+            screens.append(score_screen)
+            displaycredits(score_screen)
+            score_screen.pack(fill=BOTH, expand=True)
 
-        screen_title = Label(score_screen,
-                             text="Highscores", font=("Arial", 30, "bold"),
-                             fg="green", bg="black")
-        screen_title.place(relx=0.5, rely=0.15, anchor=CENTER)
+            screen_title = Label(score_screen,
+                                text="Highscores", font=("Arial", 30, "bold"),
+                                fg="green", bg="black")
+            screen_title.place(relx=0.5, rely=0.15, anchor=CENTER)
 
-        mode_label = Label(score_screen,
-                           text=mode, font=("Arial", 15, "bold"),
-                           fg="light green", bg="black")
-        mode_label.place(relx=0.5, rely=0.23, anchor=CENTER)
+            mode_label = Label(score_screen,
+                            text=mode, font=("Arial", 15, "bold"),
+                            fg="light green", bg="black")
+            mode_label.place(relx=0.5, rely=0.23, anchor=CENTER)
 
-        def prevmode():
-            showmode(mode_no - 1)
-        def nextmode():
-            showmode(mode_no + 1)
-        previous_button = Button(score_screen,
-                                 borderwidth=0,
-                                 text="<", font=("Arial", 10, "bold"),
-                                 fg="white", bg="black", activebackground="light grey",
-                                 height=1, width=1,
-                                 command=prevmode)
-        previous_button.place(relx=0.3, rely=0.23, anchor=CENTER)
+            def prevmode():
+                showmode(mode_no - 1, filter_player)
+            def nextmode():
+                showmode(mode_no + 1, filter_player)
+            previous_button = Button(score_screen,
+                                    borderwidth=0,
+                                    text="<", font=("Arial", 10, "bold"),
+                                    fg="white", bg="black", activebackground="light grey",
+                                    height=1, width=1,
+                                    command=prevmode)
+            previous_button.place(relx=0.3, rely=0.23, anchor=CENTER)
 
-        next_button = Button(score_screen,
-                             borderwidth=0,
-                             text=">", font=("Arial", 10, "bold"),
-                             fg="white", bg="black", activebackground="light grey",
-                             height=1, width=1,
-                             command=nextmode)
-        next_button.place(relx=0.7, rely=0.23, anchor=CENTER)
+            next_button = Button(score_screen,
+                                borderwidth=0,
+                                text=">", font=("Arial", 10, "bold"),
+                                fg="white", bg="black", activebackground="light grey",
+                                height=1, width=1,
+                                command=nextmode)
+            next_button.place(relx=0.7, rely=0.23, anchor=CENTER)
 
-        rank_header = Label(score_screen,
-                            text="Rank", font=("Arial", 15, "bold"),
-                            fg="grey", bg="black")
-        rank_header.place(relx=0.3, rely=0.3, anchor=CENTER)
-
-        score_header = Label(score_screen,
-                             text="Score", font=("Arial", 15, "bold"),
-                             fg="grey", bg="black")
-        score_header.place(relx=0.7, rely=0.3, anchor=CENTER)
-
-        back_button = Button(score_screen,
-                             borderwidth=3, relief=RIDGE,
-                             text="Back", font=("Arial", 15, "bold"),
-                             fg="white", bg="grey", activebackground="light grey",
-                             width=5,
-                             command=displaymenu)
-        back_button.place(relx=0.03, rely=0.9)
-
-        if len(scores[mode]) == 0:
-            scores[mode] = ["N.A"] * 3 # Fill with placeholders
-
-        rank_map = ["gold", "thistle3", "sienna4"]
-
-        for index in range(len(scores[mode])):
-            score = scores[mode][index]
-            rank = index + 1
-            if rank <= len(rank_map):
-                color = rank_map[rank - 1]
-            else:
-                color = "white"
             rank_header = Label(score_screen,
-                                text=rank, font=("Arial", 15, "bold"),
-                                fg=color, bg="black")
-            rank_header.place(relx=0.3, rely=0.3 + rank * 0.075, anchor=CENTER)
+                                text="Rank", font=("Arial", 15, "bold"),
+                                fg="grey", bg="black")
+            rank_header.place(relx=0.2, rely=0.3, anchor=CENTER)
 
             score_header = Label(score_screen,
-                                 text=score, font=("Arial", 15, "bold"),
-                                 fg=color, bg="black")
-            score_header.place(relx=0.7, rely=0.3 + rank * 0.075, anchor=CENTER)
+                                text="Score", font=("Arial", 15, "bold"),
+                                fg="grey", bg="black")
+            score_header.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-        if scores[mode] == ["N.A"] * 3:
-            scores[mode] = [] # Placeholders only temporary
-    showmode(0)
+            name_header = Label(score_screen,
+                                text="Player", font=("Arial", 15, "bold"),
+                                fg="grey", bg="black")
+            name_header.place(relx=0.8, rely=0.3, anchor=CENTER)
+
+
+            back_button = Button(score_screen,
+                                borderwidth=3, relief=RIDGE,
+                                text="Back", font=("Arial", 15, "bold"),
+                                fg="white", bg="grey", activebackground="light grey",
+                                width=5,
+                                command=displaymenu)
+            back_button.place(relx=0.03, rely=0.9)
+
+            if len(scores[mode]) == 0:
+                scores[mode] = ["N.A"] * 3 # Fill with placeholders
+
+            rank_map = ["gold", "thistle3", "sienna4"]
+
+            placed = 0
+            for index in range(len(sorted_scores)):
+                score = sorted_scores[index]
+                player = scores[mode][score]
+                if filter_player and player != filter_player:
+                    continue
+                rank = index + 1
+                offset = (placed + 1) * 0.075
+                if rank <= len(rank_map):
+                    color = rank_map[rank - 1]
+                else:
+                    color = "white"
+                rank_no = Label(score_screen,
+                                    text=rank, font=("Arial", 15, "bold"),
+                                    fg=color, bg="black")
+                rank_no.place(relx=0.2, rely=0.3 + offset, anchor=CENTER)
+
+                score = Label(score_screen,
+                                    text=score, font=("Arial", 15, "bold"),
+                                    fg=color, bg="black")
+                score.place(relx=0.5, rely=0.3 + offset, anchor=CENTER)
+
+                def filtergen(player):
+                    def filtermode():
+                        if filter_player == player:
+                            showmode(mode_no, None)
+                        else:
+                            showmode(mode_no, player)
+                    return filtermode
+
+                name = Button(score_screen,
+                                text=player, font=("Arial", 15, "bold"),
+                                fg=color, bg="black",
+                                command=filtergen(player))
+                name.place(relx=0.8, rely=0.3 + offset, anchor=CENTER)
+
+                index = index + 1
+                placed = placed + 1
+
+            if scores[mode] == ["N.A"] * 3:
+                scores[mode] = [] # Placeholders only temporary
+    showmode(0, None)
 
 def displaymenu():
-    global window, scores
+    global window
     clearscreens()
 
     menu = Frame(window, bg="black")
@@ -221,6 +267,14 @@ def displaymenu():
         n += 1
 
     # Intialise scores
-    if scores == {}:
+    with open("src/scores.json", "r+") as scores_file:
+        if scores_file.read() == "":
+            scores_file.write("{}") # Empty scores
+        scores_file.seek(0)
+        scores = json.load(scores_file)
         for mode in GAME_MODES:
-            scores[mode] = []
+            if mode not in scores:
+                scores[mode] = {}
+        scores_file.seek(0)
+        scores_file.truncate()
+        json.dump(scores, scores_file)
